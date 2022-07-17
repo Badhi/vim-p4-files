3998c4f960bb12535d0714daf04680ab51805612
badhi <bmadhikarinayake@gmail.com>
2022-07-17 18:48:56 +0530

    fixed command not found error in telescope integration

--- lua/telescope/_extensions/vim_p4_files.lua
+++ lua/telescope/_extensions/vim_p4_files.lua
@@ -2,6 +2,6 @@ local p4_get_files_pickers = require'telescope._extensions.vim_p4_files.pickers'
 
 return require'telescope'.register_extension {
     exports = {
-        p4_get_files = p4_get_files_pickers.p4_files
+        vim_p4_files = p4_get_files_pickers.p4_files
     }
 }
