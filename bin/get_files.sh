8dbbace0f56ad5b03d1891ebd715141f370dd151
Sarath <chandallasarath@gmail.com>
2023-10-24 11:09:17 -0700

    use p4 have to fetch file list
    Use p4 have to get local client path. This would be faster than using p4 files.

--- bin/get_files.sh
+++ bin/get_files.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-function normalize()
-{
-    if [ "%{1: -1}" == "/" ] ; then
-        echo $1
-    else
-        echo ${1}/
-    fi
-}
- 
-client_name=`p4 -F %client% info`
-root_dir=`p4 -F %clientRoot% -ztag info`
-root_dir=$(normalize $root_dir)
-
-p4 -F %depotFile% files -e //$client_name/... |\
-    p4 -F %localPath% -x - where
+p4 -F %path% -ztag have
